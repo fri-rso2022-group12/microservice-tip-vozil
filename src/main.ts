75f7d97bca9de97f4ea6637184f950d10a4e14ea
Martin Dagarin <martin.dagarin@gmail.com>
2022-10-22 23:43:41 +0200

    Added middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import * as compression from 'compression';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  
+
   // Get configuration
   const port = configService.get<number>('PORT');
+
+  // Middlewares
+  app.use(compression());
+  app.use(helmet());
+  app.enableCors();
+  app.enableShutdownHooks();
 
   await app.listen(port);
 }
