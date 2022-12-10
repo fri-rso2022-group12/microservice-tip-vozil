99e45f89f0f1d78fb2a579a8434b3e68c32efb21
Martin Dagarin <martin.dagarin@gmail.com>
2022-12-10 20:11:01 +0100

    Added Consul config server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PrometheusModule } from "@willsoto/nestjs-prometheus";
+import { ConsulModule } from 'nestjs-consul';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConsulConfigService } from './custom-config/consul-config.service';
 import { CustomConfigModule } from './custom-config/custom-config.module';
 import { DatabaseConfigService } from './custom-config/database-config.service';
 import { HealthModule } from './health/health.module';
+import { MaintenanceMiddleware } from './maintenance.middleware';
 import { ModelVozilaModule } from './model-vozila/model-vozila.module';
 import { ProizvajalecModule } from './proizvajalec/proizvajalec.module';
 
@@ -19,10 +22,24 @@ import { ProizvajalecModule } from './proizvajalec/proizvajalec.module';
     }),
     HealthModule,
     PrometheusModule.register(),
+    ConsulModule.forRootAsync({
+      imports: [CustomConfigModule],
+      inject: [ConsulConfigService],
+      useFactory: async (consulConfigService: ConsulConfigService) => {
+				return consulConfigService.getConfig();
+			},
+    }),
     ModelVozilaModule,
     ProizvajalecModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(MaintenanceMiddleware)
+      .exclude('health/(.*)')
+      .forRoutes('*');
+  }
+}
